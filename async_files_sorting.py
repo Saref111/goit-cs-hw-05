f943c60bf450cb25357efb97361a45cf28f6ceef
Saref111 <saref012@gmail.com>
2024-04-27 22:17:42 +0300

    added first task

--- async_files_sorting.py
+++ async_files_sorting.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from aiopath import AsyncPath
+from aioshutil import copyfile
 import logging
 
 logging.basicConfig(filename='file_sorting.log', level=logging.INFO, 
@@ -7,19 +8,21 @@ logging.basicConfig(filename='file_sorting.log', level=logging.INFO,
 
 async def read_folder(input_dir):
     async for file in input_dir.iterdir():
-        if file.is_file():
+        if await file.is_file():
+            print(f'Found file: {file.name}')
             yield file
 
 async def copy_file(file, output_dir):
     subfolder_name = file.suffix[1:]
     subfolder = output_dir / subfolder_name
     await subfolder.mkdir(exist_ok=True)
-    await file.copy(subfolder / file.name)
+    destination = subfolder / file.name
+    await copyfile(file, destination)
 
 
 async def main(input_dir, output_dir):
     async for file in read_folder(input_dir):
-        await copy_file(file, output_dir)
+        await copy_file(file, output_dir)       
 
 if __name__ == '__main__':
     import asyncio
